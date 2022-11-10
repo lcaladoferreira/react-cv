03e1662fe69eb94774b5789e3053a6e61ee0dc55
lcaladoferreira <83848600+lcaladoferreira@users.noreply.github.com>
2022-11-10 13:41:55 -0300

    lcfDeploy

---
+++ src/Components/Contact.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Fade, Slide } from "react-reveal";
+
+class Contact extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const name = this.props.data.name;
+    const email = this.props.data.email;
+    const phone = this.props.data.phone;
+    const message = this.props.data.contactmessage;
+
+    return (
+      <section id="contact">
+
+
+        <div className="row">
+          <Slide left duration={1000}>
+            <div className="eight columns">
+              <form action="mailto:lcaladoferreira@gmail.com" method="post" id="contactForm" name="contactForm" >
+                <fieldset>
+                  <div>
+                    <button className="submit">click to send me an e-mail</button>
+                    <span id="image-loader">
+                      <img alt="" src="images/loader.gif" />
+                    </span>
+                  </div>
+                </fieldset>
+              </form>
+            </div>
+          </Slide>
+
+          <Slide right duration={1000}>
+            <aside className="four columns footer-widgets">
+              <div className="widget widget_contact">
+                <h4>My Contacts</h4>
+                <p className="address">
+                  {name}
+                  <br />
+                  {email} 
+                  <br />
+                  <span>{phone}</span>
+                </p>
+              </div>
+            </aside>
+          </Slide>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Contact;
